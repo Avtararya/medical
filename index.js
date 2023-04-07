289b15bd1a3ce569937a396058c00aa79422bf29
Avtararya <100572234+Avtararya@users.noreply.github.com>
2023-04-07 22:23:33 +0530

    first commit

--- index.js
+++ index.js
@@ -1,122 +1,107 @@
 import express from "express";
 import bodyParser from "body-parser";
-import axios from "axios";
+// import axios from "axios";
+import cors from "cors";
+
 import dotenv from "dotenv";
 import Auth from "./routes/registrationRouter.js";
 dotenv.config();
 
-const clientId = "SBX_002395";
-const clientSecret = "f71714da-bfb6-4744-bc25-c839df86a29d";
-// const searchQuery = "John Doe";
-
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-const getAccessToken = async (clientId, clientSecret) => {
-  const response = await axios.post(
-    "https://dev.abdm.gov.in/gateway/v0.5/sessions",
-    {
-      clientId: clientId,
-      clientSecret: clientSecret,
-    }
-  );
-  return response.data.accessToken;
-};
-
-const accessToken = await getAccessToken(clientId, clientSecret);
 // const searchResults = await searchPatients(accessToken, searchQuery);
 // console.log(accessToken);
 
 //Update bridge URL:
 
+// const bridgeUrl = "https://your-hip-or-hiu-service-url";
 
-const bridgeUrl = "https://your-hip-or-hiu-service-url";
+// const updateBridgeUrl = async (accessToken) => {
+//   try {
+//     const response = await axios.patch(
+//       "https://dev.ndhm.gov.in/devservice/v1/bridges",
+//       {
+//         url: bridgeUrl,
+//       },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${accessToken}`,
+//           "Content-Type": "application/json",
+//           Accept: "*/*",
+//         },
+//       }
+//     );
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
-const updateBridgeUrl = async (accessToken) => {
-  try {
-    const response = await axios.patch(
-      "https://dev.ndhm.gov.in/devservice/v1/bridges",
-      {
-        url: bridgeUrl,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-          Accept: "*/*",
-        },
-      }
-    );
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// Call the function with the access token
-updateBridgeUrl(accessToken);
+// // Call the function with the access token
+// updateBridgeUrl(accessToken);
 
 //Add HIP service to the bridge:
 
-const addHipService = async (accessToken) => {
-  try {
-    const response = await axios.put(
-      "https://dev.ndhm.gov.in/devservice/v1/bridges/services",
-      {
-        id: "unique-hip-id",
-        name: "Your-facility-name",
-        type: "HIP",
-        active: true,
-        alias: ["EG"],
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      }
-    );
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-};
+// const addHipService = async (accessToken) => {
+//   try {
+//     const response = await axios.put(
+//       "https://dev.ndhm.gov.in/devservice/v1/bridges/services",
+//       {
+//         id: "unique-hip-id",
+//         name: "Your-facility-name",
+//         type: "HIP",
+//         active: true,
+//         alias: ["EG"],
+//       },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${accessToken}`,
+//           "Content-Type": "application/json",
+//           Accept: "application/json",
+//         },
+//       }
+//     );
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
-// Call the function with the access token
-addHipService(accessToken);
+// // Call the function with the access token
+// addHipService(accessToken);
 
 //Add HIU service to the bridge:
 
-const addHiuService = async (accessToken) => {
-  try {
-    const response = await axios.put(
-      "https://dev.ndhm.gov.in/devservice/v1/bridges/services",
-      {
-        id: "unique-hiu-id",
-        name: "Your-facility-name",
-        type: "HIU",
-        active: true,
-        alias: ["EG"],
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      }
-    );
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-};
+// const addHiuService = async (accessToken) => {
+//   try {
+//     const response = await axios.put(
+//       "https://dev.ndhm.gov.in/devservice/v1/bridges/services",
+//       {
+//         id: "unique-hiu-id",
+//         name: "Your-facility-name",
+//         type: "HIU",
+//         active: true,
+//         alias: ["EG"],
+//       },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${accessToken}`,
+//           "Content-Type": "application/json",
+//           Accept: "application/json",
+//         },
+//       }
+//     );
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
-// Call the function with the access token
-addHiuService(accessToken);
+// // Call the function with the access token
+// addHiuService(accessToken);
 
 app.use("/api", Auth);
 
--- routes/registrationRouter.js
+++ routes/registrationRouter.js
@@ -4,86 +4,108 @@ import request from "request";
 
 const router = express.Router();
 
-const baseUrl = "https://healthidsbx.ndhm.gov.in/api";
+const clientId = "SBX_002395";
+const clientSecret = "f71714da-bfb6-4744-bc25-c839df86a29d";
+// const searchQuery = "John Doe";
+
+const getAccessToken = async (clientId, clientSecret) => {
+  const response = await axios.post(
+    "https://dev.abdm.gov.in/gateway/v0.5/sessions",
+    {
+      clientId: clientId,
+      clientSecret: clientSecret,
+    }
+  );
+  return response.data.accessToken;
+};
+
+const accessToken = await getAccessToken(clientId, clientSecret);
+
+console.log(accessToken);
+
+const BaseURI = "https://healthidsbx.abdm.gov.in";
+const BasePath = "api";
 
 // Generate Mobile OTP to start registration
+
 router.post("/generateOtp", async (req, res) => {
   try {
-    request
-      .post(
-        "https://healthidsbx.ndhm.gov.in/api/v1/registration/mobile/generateOtp",
-        JSON.stringify({
-          mobile: "+91-7387791658",
-        })
-      )
-      .on("response", function (response) {
-        res.json(response);
-        console.log(response.statusCode); // 200
-        console.log(response.headers["content-type"]); // 'image/png'
-      });
+    const { mobile } = req.body;
+    const options = {
+      url: `${BaseURI}/${BasePath}/v1/registration/mobile/generateOtp`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      json: { mobile },
+    };
+    request.post(options, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong." });
+      } else {
+        res.json(body);
+      }
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Something went wrong." });
   }
 });
 
-// router.post("/generateOtp", async (req, res) => {
-//   try {
-//     const { mobileNumber } = req.body;
+router.post("/resendOtp", async (req, res) => {
+  try {
+    const { mobileNumber } = req.body;
 
-//     const response = await axios.post(`${baseUrl}/generateOtp`, {
-//       mobileNumber,
-//     });
+    const response = await axios.post(
+      `${BaseURI}/${BasePath}/api/v1/registration/mobile/resendOtp`,
+      {
+        mobileNumber,
+      }
+    );
 
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Something went wrong." });
-//   }
-// });
-
-// Resend Mobile OTP for Health ID registration
-// router.post("/resendOtp", async (req, res) => {
-//   try {
-//     const { mobileNumber } = req.body;
-
-//     const response = await axios.post(`${baseUrl}/resendOtp`, {
-//       mobileNumber,
-//     });
-
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Something went wrong." });
-//   }
-// });
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong." });
+  }
+});
 
 // // Verify Mobile OTP sent as part of registration transaction
-// router.post("/verifyOtp", async (req, res) => {
-//   try {
-//     const { mobileNumber, otp } = req.body;
+router.post("/verifyOtp", async (req, res) => {
+  try {
+    const { txnId, otp } = req.body;
 
-//     const response = await axios.post(`${baseUrl}/verifyOtp`, {
-//       mobileNumber,
-//       otp,
-//     });
-
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Something went wrong." });
-//   }
-// });
+    const response = await axios.post(
+      `${BaseURI}/${BasePath}/v1/registration/mobile/verifyOtp`,
+      {
+        otp,
+        txnId,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong." });
+  }
+});
 
 // // Verify Aadhaar OTP Only
 // router.post("/aadhaar/verifyOTP", async (req, res) => {
 //   try {
 //     const { aadhaarNumber, otp } = req.body;
 
-//     const response = await axios.post(`${baseUrl}/aadhaar/verifyOTP`, {
-//       aadhaarNumber,
-//       otp,
-//     });
+//     const response = await axios.post(
+//       `${ BaseURI }/${ BasePath }/v1/registration/mobile/verifyOtp`,
+//       {
+//         aadhaarNumber,
+//         otp,
+//       }
+//     );
 
 //     res.json(response.data);
 //   } catch (error) {
@@ -93,20 +115,20 @@ router.post("/generateOtp", async (req, res) => {
 // });
 
 // // Create Health ID with verified mobile token
-// router.post("/createHealthId", async (req, res) => {
-//   try {
-//     const { mobileNumber, token } = req.body;
+router.post("/createHealthId", async (req, res) => {
+  try {
+    const { mobileNumber, token } = req.body;
 
-//     const response = await axios.post(`${baseUrl}/createHealthId`, {
-//       mobileNumber,
-//       token,
-//     });
+    const response = await axios.post(`${BaseURI}/createHealthId`, {
+      mobileNumber,
+      token,
+    });
 
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Something went wrong." });
-//   }
-// });
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong." });
+  }
+});
 
 export default router;
